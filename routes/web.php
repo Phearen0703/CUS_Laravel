398b8a7db8e4df7a0e823eecb08ee460d195e4ac
Phearen <phearen0703@gmail.com>
2025-07-07 12:47:48 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('backends.home.index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
