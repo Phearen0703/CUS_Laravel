45bd1fe780cf67816b7eebb8cf9fcbaa171055fe
CHOUB PHEAREN <phearensmos9@gmail.com>
2025-09-14 16:36:29 +0700

    fix modal pdf view

--- resources/views/frontends/books/book-modal.blade.php
+++ resources/views/frontends/books/book-modal.blade.php
@@ -1,26 +1,23 @@
 <div class="row">
     <div class="col-md-5">
-        <img src="{{ asset($book->image ?? 'images/no-image.png') }}" 
-             class="img-fluid rounded" 
-             alt="{{ $book->title }}">
+        <img src="{{ asset($book->image ?? 'images/no-image.png') }}" class="img-fluid rounded"
+            alt="{{ $book->title }}">
     </div>
     <div class="col-md-7">
         <h4>{{ $book->title }}</h4>
         <p><strong>Author:</strong> {{ $book->author }}</p>
         <p><strong>Category:</strong> {{ $book->category->name ?? 'N/A' }}</p>
-        <p><strong>Published:</strong> {{ $book->published_at ?? 'Unknown' }}</p>
+        <p><strong>Published:</strong> {{ $book->published_date ?? 'Unknown' }}</p>
+        <p><strong>Book Code:</strong> {{ $book->code ?? 'Unknown' }}</p>
         <p>{{ $book->description ?? 'No description available.' }}</p>
 
         <div class="mt-3">
             @if($book->pdf)
-                <a href="{{ route('books.viewPdf', $book->id) }}" 
-                class="btn btn-success">
-                View PDF
+                <a href="{{ asset($book->pdf) }}" target="_blank" class="btn btn-sm btn-primary">
+                    <i class="fa-solid fa-file-pdf"></i> View PDF
                 </a>
             @else
-                <button class="btn btn-secondary" disabled>
-                    No PDF Available
-                </button>
+                <span class="text-muted">No PDF</span>
             @endif
         </div>
 
